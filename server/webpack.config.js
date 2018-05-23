e621684b7f34f7b7ec491968385725321b2d1b35
Ocasio, Juan A <jaocasio@brandsafway.com>
2018-05-23 07:26:21 -0400

    Added prettierrc file

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -1,20 +1,20 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
   entry: {
-    main: "./src/index.js"
+    main: './src/index.js',
   },
   outout: {
-    filename: "[name].[hash].js",
-    path: path.resolve("./dist")
+    filename: '[name].[hash].js',
+    path: path.resolve('./dist'),
   },
   modules: {
     rules: [
       {
         test: /\.js$/,
-        exclude: ["node_modules"],
-        use: [{ loader: "babel-loader"}],
-      }
-    ]
-  }
+        exclude: ['node_modules'],
+        use: [{ loader: 'babel-loader' }],
+      },
+    ],
+  },
 };
